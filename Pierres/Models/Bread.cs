fb9be64ce8024672d5cad27d0b087afb579704fb
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-11 11:11:46 -0800

    wroted method to calculate pricing accounting for buy 2 get 1

--- Pierres/Models/Bread.cs
+++ Pierres/Models/Bread.cs
@@ -10,8 +10,16 @@ namespace Pierres
     } 
     public int LoafPricing()
     {
+      if (LoafCount == 1) 
+      {
       int price = LoafCount * 5;
       return price;
+      }
+      else 
+      {
+        int price = ((LoafCount/3) * 10) + ((LoafCount % 3) * 5);
+        return price;
+      }
     }
 
   }
