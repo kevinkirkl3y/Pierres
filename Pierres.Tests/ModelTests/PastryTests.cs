429454679df46c5083f31f9206e8e273b9d3f4c4
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-11 11:28:45 -0800

    create test for checking that Pastry object correctly returns pastryCount

--- Pierres.Tests/ModelTests/PastryTests.cs
+++ Pierres.Tests/ModelTests/PastryTests.cs
@@ -12,5 +12,13 @@ namespace Pierres.Tests
       Pastry newOrder = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newOrder.GetType());
     }
+    [TestMethod]
+    public void Pastry_ReturnsNumberOfPastries_Int()
+    {
+      int pastryCount = 3;
+      Pastry newOrder = new Pastry(pastryCount);
+      int result = newOrder.PastryCount;
+      Assert.AreEqual(result, pastryCount);
+    }
   }
 }
