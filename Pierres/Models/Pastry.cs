4356e452ebe11d9612aac70149af9b42b0d2c4fb
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-11 11:22:52 -0800

    Create constructor for Pastry Object taking in PastryCount argument.

--- Pierres/Models/Pastry.cs
+++ Pierres/Models/Pastry.cs
@@ -2,6 +2,11 @@ namespace Pierres
 {
   public class Pastry
   {
+    public int PastryCount { get; set; }
     
+    public Pastry(int aPastryCount)
+    {
+      PastryCount = aPastryCount;
+    }
   }
 }
