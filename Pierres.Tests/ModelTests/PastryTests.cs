b2684a2edcd25ab2f6dbd324d0f3a7c075eb2ab8
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-11 11:23:59 -0800

    modified test to account for constructor taking in PastryCount argument.

--- Pierres.Tests/ModelTests/PastryTests.cs
+++ Pierres.Tests/ModelTests/PastryTests.cs
@@ -9,7 +9,7 @@ namespace Pierres.Tests
     [TestMethod]
     public void Pastry_CreatesInstanceOfPastryClass_True()
     {
-      Pastry newOrder = new Pastry();
+      Pastry newOrder = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newOrder.GetType());
     }
   }
