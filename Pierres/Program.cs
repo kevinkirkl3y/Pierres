0818ca60242c898cda78f842ffbc650671d48646
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-11 14:37:32 -0800

    functioning but limited UI

---
+++ Pierres/Program.cs
@@ -0,0 +1,82 @@
+using System;
+using Pierres;
+
+namespace StoreFront
+{
+
+  public class Program
+  {
+    public static void Main()
+    {
+      Greeting();
+      string response = Console.ReadLine();
+      if (response == "bread" || response == "Bread")
+      {
+        BreadOrder();
+      }
+      else if (response == "pastry" || response == "Pastry")
+      {
+        PastryOrder();
+      }
+
+
+
+
+    }
+    public static void Greeting()
+    {
+      Console.WriteLine("--------------------------------------------------------------------");
+      Console.WriteLine("Welcome to Pierres' Bakery. Purveyor of fine baguettes and pastries.");
+      Console.WriteLine("--------------------------------------------------------------------");
+      Console.WriteLine("We currently are running the following specials:");
+      Console.WriteLine("Baguettes are $5 a peice, buy 2 get one free. Pastries are $2 individually and 3 for $5.");
+      Console.WriteLine("--------------------------------------------------------------------");
+      Console.WriteLine("What would you like to order? (bread/pastry)");
+    }
+    public static void BreadOrder()
+    {
+      Console.WriteLine("Of course! How many baguettes would you like? (enter number: Ex.'5')");
+      int loafCount = int.Parse(Console.ReadLine());
+      Bread breadOrder = new Bread(loafCount);
+      Console.WriteLine("Okay, I have you down for " + loafCount + " of our fresh baguettes.");
+      Console.WriteLine("Can I get you any pastries or would you like to view your total? (pastry/total)");
+      int breadPrice = breadOrder.LoafPricing();
+      string strBreadPrice = breadPrice.ToString();
+      string response = Console.ReadLine();
+      
+      if (response == "pastry" || response == "Pastry")
+      {
+        Console.WriteLine("Your total for bread is $" + strBreadPrice);
+        PastryOrder();
+        
+      }
+      else if (response == "total" || response == "Total")
+      {
+        Console.WriteLine("Your total for bread is $" + strBreadPrice);
+      }
+      
+      
+    }
+    public static void PastryOrder()
+    {
+      Console.WriteLine("Of course! How many pastries would you like? (enter number: Ex.'5')");
+      int pastryCount = int.Parse(Console.ReadLine());
+      Pastry pastryOrder = new Pastry(pastryCount);
+      Console.WriteLine("Okay, I have you down for " + pastryCount + " of our fresh pastries.");
+      Console.WriteLine("Can I get you any baguettes or would you like to view your total? (bread/total)");
+      string response = Console.ReadLine();
+      int pastryPrice = pastryOrder.PastryPricing();
+      string strPastryPrice = pastryPrice.ToString();
+      if (response == "bread" || response == "Bread")
+      {
+        Console.WriteLine("Your total for pastries is $" + strPastryPrice) ;
+        BreadOrder();
+        
+      }
+      else if (response == "total" || response == "Total")
+      {
+        Console.WriteLine("Your total for pastries is $" + strPastryPrice) ;
+      }
+    }
+  }
+}
